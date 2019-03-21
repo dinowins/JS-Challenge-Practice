81d40ab541050cafb5df875c3bc7f0ebcf094319
Dino Nguyen <dinodnguyen@gmail.com>
2019-03-21 15:06:35 -0700

    Add test for fromListToObject function

--- spec/challenges-spec.js
+++ spec/challenges-spec.js
@@ -11,4 +11,14 @@ describe('Challenges', function() {
   it("should return an array of all the input object's keys", function() {
     expect(getAllKeys(obj)).toEqual(['name', 'age', 'hasPets']);
   });
+
+  const arr = [
+    ['make', 'Ford'],
+    ['model', 'Mustang'],
+    ['year', 1964]
+  ];
+
+  it("should take in an array of arrays and return an object with each pair of elements in the array as a key-value pair", function() {
+    expect(fromListToObject(arr)).toEqual({make:"Ford", model: "Mustang", year: "1964"});
+  });
 });
